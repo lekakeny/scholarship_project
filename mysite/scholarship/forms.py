4bff7d0f94a9ab0ebcc1afef7e3624aea4dbc37b
lekakeny <lekakeny.karoi@gmail.com>
2020-08-07 15:50:19 +0300

    push changes

--- mysite/scholarship/forms.py
+++ mysite/scholarship/forms.py
@@ -4,12 +4,21 @@ from . import models
 
 # bio-data
 class BioDataForm(forms.Form):
-    pass
+    your_name = forms.CharField(label='Your name', max_length=100)
+    your_address = forms.CharField(label='Your Address', max_length=100)
+    your_mobile = forms.CharField(label='Your Mobile Number', max_length=100)
+    your_email = forms.CharField(label='Your Email Address', max_length=100)
+    your_birth = forms.FileField(label='Your Birth Certificate', max_length=100)
+    your_naid = forms.FileField(label='Your National ID', max_length=100)
 
 
 class SchoolForm(forms.Form):
-    pass
+    school_name = forms.CharField(label='Name of your School', max_length=100)
+    school_address = forms.CharField(label='Your School Address', max_length=100)
+    academic_level = forms.CharField(label='Your Academic Level', max_length=100)
+    completion_year = forms.DateTimeField(label='Your Expected Year of Completion', max_length=100)
 
 
 class ReasonForm(forms.Form):
-    pass
+    sponsorship_reasons = forms.CharField(label='In at most 500 words, write an essay on why you need this sponsorship', max_length=1000)
+    recommendation_letter = forms.FileField(label='Upload your letter of recommendation', max_length=100)
